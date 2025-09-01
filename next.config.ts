b3f509763e63bebd72bdaac4d5f25e7d82f5d0fd
Aaron <montiveroaaron@gmail.com>
2025-09-01 11:59:32 -0300

    change next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /LICENSE$/,
+      type: "asset/source", // lo trata como texto plano
+    })
+  }
 };
 
 export default nextConfig;
